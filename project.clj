1615feb67188802c092a2723c963238fa7dabce3
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-05-29 10:41:08 +0200

    Add send function #2

--- project.clj
+++ project.clj
@@ -5,11 +5,13 @@
             :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[cheshire "5.8.0"]
+                 [clojure.java-time "0.3.2"]
+                 [funcool/struct "1.2.0"]
                  [http-kit "2.3.0"]
-                 [com.cemerick/url "0.1.1"]]
+                 [pandect "0.6.1"]]
 
-  :profiles {:dev {:plugins [[lein-cloverage "1.0.10"]
-                             [lein-kibit "0.1.6"]]
-                   :dependencies [[clj-http-fake "1.0.3"]
+  :profiles {:dev {:plugins [[lein-kibit "0.1.6"]
+                             [venantius/ultra "0.5.2"]]
+                   :dependencies [[http-kit.fake "0.2.1"]
                                   [org.clojure/clojure "1.9.0"]]
                    :source-paths ["src" "dev/src"]}})
